4d2aa529995fa7245f02e22260a5b571acbdad6d
hungrybeagle <cyang@deltalearns.ca>
2021-12-08 14:11:59 -0800

    Update and rename 04-game.py to x04_game.py

--- x04_game.py
+++ x04_game.py
@@ -5,7 +5,7 @@ Create a full rock paper scissors game
 You can incorporate the other files in this project to help you
 """
 
-from 03winner import *
-from 02computer import *
-from 01player import *
+from x01_player import *
+from x02_computer import *
+from x03_choice import *
 
