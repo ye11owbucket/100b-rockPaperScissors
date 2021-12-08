06d4eced3051f6b6703f22f20520299c5bf8bb3f
hungrybeagle <cyang@deltalearns.ca>
2021-12-08 14:12:32 -0800

    Update x04_game.py

--- x04_game.py
+++ x04_game.py
@@ -7,5 +7,8 @@ You can incorporate the other files in this project to help you
 
 from x01_player import *
 from x02_computer import *
-from x03_choice import *
+from x03_winner import *
+
+if __name__ == "__main__":
+  pass
 
