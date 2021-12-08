250955deddd01b5671621418a3cde72274fad6f2
hungrybeagle <cyang@deltalearns.ca>
2021-12-08 14:04:15 -0800

    Update and rename 04-check-conflicts.py to 04-game.py

---
+++ 04-game.py
@@ -0,0 +1,11 @@
+#!python3
+
+"""
+Create a full rock paper scissors game
+You can incorporate the other files in this project to help you
+"""
+
+from 03winner import *
+from 02computer import *
+from 01player import *
+
