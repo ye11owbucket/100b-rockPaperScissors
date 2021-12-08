401b2953f0fbc85a56a5cf1c19c75b08852ab132
hungrybeagle <cyang@deltalearns.ca>
2021-12-08 13:52:47 -0800

    Update and rename 02-convert.py to 02-computer.py

---
+++ 02-computer.py
@@ -0,0 +1,22 @@
+#!python3
+"""
+Create a function that creates a random choice for the computer player:
+input parameters: none required
+output:
+
+0 : rock
+1 : paper
+2 : scissors
+"""
+
+def computerChoice():
+  
+  
+  
+  
+  return value
+
+
+if __name__ == "__main__":
+  computer = computerChoice()
+  print(computer)
