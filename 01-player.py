6212622ccdaab8b09dd0266cbd878d9602832d70
hungrybeagle <cyang@deltalearns.ca>
2021-12-08 13:51:07 -0800

    Update and rename 01-map.py to 01-player.py

---
+++ 01-player.py
@@ -0,0 +1,32 @@
+#! python3
+
+"""
+Create a function that asks the player for their choice
+The function should return:
+  rock     : 0
+  paper    : 1
+  scissors : 2
+
+```
+Sample Run:
+Enter your choice:
+rock
+
+Output: 0
+"""
+
+def playerChoice():
+  '''
+  No input parameters needed.
+  Function should ask the players to make their choice.  How you ask is unimportant, but the
+  output must be consistent:
+  0: rock
+  1: paper
+  2: scissors
+  '''
+  return value
+
+
+if __name__ == "__main__":
+  player = playerChoice()
+  print(player)
